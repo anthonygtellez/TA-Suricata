590e2f845673a5d3db1ed6a5897df558056f3f42
Anthony Tellez <anthonygtellez@gmail.com>
2016-10-12 14:58:52 -0500

    Change directory not needed for configure

--- appserver/static/docs/install_suricata.sh
+++ appserver/static/docs/install_suricata.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+apt update
 preinstall="sudo apt-get -y install libpcre3 libpcre3-dbg libpcre3-dev \
 build-essential autoconf automake libtool libpcap-dev libnet1-dev \
 libyaml-0-2 libyaml-dev zlib1g zlib1g-dev libcap-ng-dev libcap-ng0 \
@@ -7,13 +8,12 @@ ${preinstall}
 wget "http://www.openinfosecfoundation.org/download/suricata-3.1.tar.gz"
 ### Download Suricata 3.1 and configure directories
 tar -xzf suricata-3.1.tar.gz
-cd suricata-3.1
-configure="./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var "
+configure="suricata-3.1/configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var "
 ${configure}
 ### Compile from Source
 make
-sudo make install-full
-sudo ldconfig
+make install-full
+ldconfig
 
 ### Enable Emerging Threats
 sudo cp classification.config /etc/suricata
